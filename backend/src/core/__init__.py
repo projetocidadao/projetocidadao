ac84972fe7cd042c73661db996163c49349a03f0
projetocidadao <3ilhas@protonmail.com>
2026-06-15 21:51:06 -0300

    build: exporta storage no pacote core

--- backend/src/core/__init__.py
+++ backend/src/core/__init__.py
@@ -10,16 +10,24 @@ from src.core.deps import (
     require_moderator,
     require_avancado,
 )
+from src.core.storage import (
+    calcular_hash_sha256,
+    detectar_mime,
+    mime_para_tipo,
+    gerar_chave_arquivo,
+    validar_arquivo,
+    salvar_arquivo,
+    gerar_url_publica,
+    deletar_arquivo,
+    abrir_arquivo_local,
+    MIME_ALLOWED,
+)
 
 __all__ = [
-    "hash_senha",
-    "verificar_senha",
-    "criar_access_token",
-    "decodificar_token",
-    "get_current_user",
-    "get_current_active_user",
-    "require_role",
-    "require_admin",
-    "require_moderator",
-    "require_avancado",
+    "hash_senha", "verificar_senha", "criar_access_token", "decodificar_token",
+    "get_current_user", "get_current_active_user", "require_role",
+    "require_admin", "require_moderator", "require_avancado",
+    "calcular_hash_sha256", "detectar_mime", "mime_para_tipo",
+    "gerar_chave_arquivo", "validar_arquivo", "salvar_arquivo",
+    "gerar_url_publica", "deletar_arquivo", "abrir_arquivo_local", "MIME_ALLOWED",
 ]
